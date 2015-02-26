f1aeb7c4fb345df1052931884c2ece4acd7f52f8
Patrick Teulings <patrickteulings@gmail.com>
2015-02-26 18:24:41 +0100

    Code clean up
    Removed some code from an old projects

--- dist/js/app.js
+++ dist/js/app.js
@@ -2291,45 +2291,9 @@ Templates = (function(){
 
 Socials = (function(){
 
-    // AIzaSyBx-Su80zi4ySOQNi2QBYggIMfOdvH7mog
-
+    
     var obj = {};
 
-
-
-    obj.getShortUrl = function(_data,_callback){
-        var requestData = _data;
-
-        var request = gapi.client.urlshortener.url.insert({
-            'resource': {
-                'longUrl': requestData.longURL
-            }
-        });
-
-        request.execute(function(response){
-
-            if(response.id !== null){
-                requestData.shortURL = response.id;                
-                _callback(requestData);
-            } 
-            else{
-                // FAILED, USE UNSHORTENED URL
-                requestData.shortURL = requestData.longURL;                
-                _callback(requestData);
-            }
-        });
-
-    };
-
-
-    function initShortner() {
-        gapi.client.setApiKey('AIzaSyBx-Su80zi4ySOQNi2QBYggIMfOdvH7mog');
-        gapi.client.load('urlshortener', 'v1').then(function(){});
-    }
-
-    setTimeout(function(){
-        initShortner();
-    },2000);
 
 
 
@@ -2347,25 +2311,14 @@ Socials = (function(){
     // TWITTER
     //
 
-    obj.shareOnTwitter = function(_data){
+    obj.shareOnTwitter = function(_url){
 
-        var shareData = _data;
-        shareData.longURL = 'http://ezelsbruggetje.nl/' + shareData.id;
 
-        // GET THE SHORT URL
-        
-        if(_data.shortURL === undefined){
-            obj.getShortUrl(shareData, obj.shareOnTwitter);    
-            return;
-        }
-  
-        // WE GET THE TITLE OF THE LINK
-
-        var title  = escape("beschrijving bruggetje");
+        var title  = escape("example");
 
         // OPEN AND CENTER TWITTER DIALOGUE
 
-        window.open('http://twitter.com/share?url=' + _data.shortURL + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        window.open('http://twitter.com/share?url=' + _url + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
 
     };
 
@@ -2375,26 +2328,17 @@ Socials = (function(){
     //
 
 
-    obj.shareOnFacebook = function(_data){
+    obj.shareOnFacebook = function(_url){
         
-        var shareData = _data;
-        shareData.longURL = 'http://ezelsbruggetje.nl/' + shareData.id;
-
-        // GET THE SHORT URL
-        
-        if(_data.shortURL === undefined){
-            obj.getShortUrl(shareData, obj.shareOnFacebook);    
-            return;
-        }
 
 
         FB.ui({
             method: 'feed',
             name: 'This is the content of the "name" field.',
-            link: shareData.shortURL,
+            link: _url,
             picture: 'http://www.groupstudy.in/img/logo3.jpeg',
-            caption: 'Ezelsbruggetje.nl',
-            description: 'De omschrijving van het bruggetje',
+            caption: 'example',
+            description: 'De omschrijving van het example',
             message: ''
         });
     };
@@ -2405,96 +2349,21 @@ Socials = (function(){
 
 })();
 
-
-var categories = {};
-
-var latest = [];
-latest[0] = {id:'1234321',title:'Paris je t\'aime',content:'de zaanse schans schranst',author:'jelle broekhuizen',likes:0,category:'french',categoryTitle:'Frans'};
-latest[1] = {id:'1234322',title:'Tweede item',content:'de zaanse<br><br>schans schranst',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-latest[2] = {id:'1234323',title:'Derde item met een lange titel',content:'de Uber<br><br>schans schranst<br>',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-
-
-var popular = [];
-popular[0] = {id:'1234321',title:'Super populair',content:'de zaanse schans schranst',author:'jelle broekhuizen',likes:0,category:'french',categoryTitle:'Frans'};
-popular[1] = {id:'1234322',title:'Mega populair',content:'de zaanse<br><br>schans schranst',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-popular[2] = {id:'1234323',title:'Heel coolr',content:'de Uber<br><br>schans schranst<br>',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-popular[3] = {id:'1234324',title:'Super populair',content:'de zaanse schans schranst',author:'jelle broekhuizen',likes:0,category:'french',categoryTitle:'Frans'};
-popular[4] = {id:'1234325',title:'Mega populair',content:'de zaanse<br><br>schans schranst',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-popular[5] = {id:'1234326',title:'Heel coolr',content:'de Uber<br><br>schans schranst<br>',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-
-
-var dutch = [];
-dutch[0] = {id:'1234321',title:'Eerste item',content:'de zaanse schans schranst',author:'jelle broekhuizen',likes:0,category:'dutch',categoryTitle:'Nederlands'};
-dutch[1] = {id:'1234322',title:'Tweede item',content:'de zaanse<br><br>schans schranst',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-dutch[2] = {id:'1234323',title:'Derde item met een lange titel',content:'de Uber<br><br>schans schranst<br>',author:'jelle broekhuizen',likes:20,category:'dutch',categoryTitle:'Nederlands'};
-
-var geography = [];
-geography[0] = {id:'1234321',title:'Eerste item',content:'de zaanse schans schranst',author:'jelle broekhuizen',likes:0,category:'geography',categoryTitle:'Aardrijkskunde'};
-geography[1] = {id:'1234322',title:'Tweede item',content:'de zaanse<br><br>schans schranst',author:'jelle broekhuizen',likes:20,category:'geography',categoryTitle:'Aardrijkskunde'};
-geography[2] = {id:'1234323',title:'Derde item met een lange titel',content:'de Uber<br><br>schans schranst<br>',author:'jelle broekhuizen',likes:20,category:'geography',categoryTitle:'Aardrijkskunde'};
-
+//
+//  FACTORY.JS
+//
 
 
 Factory= (function(){
 
     var obj = {};
-    var categoriesLoaded = false;
 
-
-    obj.get = function(_category){
-        if(_category === 'latest'){
-            return latest;
-        }        
-        if(_category === 'dutch'){
-            return dutch;
-        }
-        if(_category === 'geography'){
-            return geography;
-        }
-        if(_category === 'popular'){
-            return popular;
-        }
-
-    };
-
-
-    obj.getCategories = function(_callback){        
-        if(Utilities.isEmptyObject(categories)) return false;
-        return categories;
-    };
-
-    obj.setCategories = function(_categories){        
-        categories = _categories;
-    };    
-
-    obj.loadCategories = function(_url){                
-        var categoryListPost = $.ajax({ url: "data/categories.json" });
-        return categoryListPost;
-    };
-
-    obj.loadMnemonics = function(_category){        
-        var mnemonicPost = $.ajax({ url: "data/" + _category + ".json" });
-        return mnemonicPost;
-    };
-
-    obj.loadUser = function(_username){
-        var userPost = $.ajax({ url: "data/" + _username + ".json" });
-        return userPost;
+    obj.example = function(_username){
+        var examplePost = $.ajax({ url: "data/" + _username + ".json" });
+        return examplePost;
     };    
 
 
-    //
-    // ADD MNEMNOIC TO USER FAVOURITES IF LOGGED IN, OR SIMPLY ADD A 'LIKE' WHEN LOGGED OUT
-    //
-
-
-    obj.addAsFavourite = function(_elementID,_favourite){
-        var postData = {};
-        postData.id = _elementID;
-        postData.favourite = _favourite;
-        var addFavourite = $.ajax({ url: "data/categories.json", data: postData });
-        return addFavourite;
-    };
 
 
 
@@ -2637,262 +2506,7 @@ MainNavigation = (function(){
 
     var obj = {};
 
-    var currentScrollTop = 0;
-
-    obj.init = function(){        
-        obj.addEvents();
-    };
-
-
-    //
-    // ADD EVENTS
-    //
-
-
-    obj.addEvents = function(){
-        $('#categoriesToggle').on('click',function(){
-            obj.openRightHandMenu();
-            obj.setActiveMenuItem($(this));
-            CategorySlideOutMenu.init();
-        });
-
-
-        $('#addMnemonicToggle').on('click',function(){            
-            if(Utilities.laravelBuild === false || Utilities.laravelBuild === true){
-                AddMnemonic.init();
-            }
-            obj.setActiveMenuItem($(this));            
-        });  
-
-
-        $('#profileToggle').on('click',function(){            
-            obj.setActiveMenuItem($(this));            
-        });  
-
-
-        $('#explanationToggle').on('click',function(){
-            obj.setActiveMenuItem();
-            obj.toggleLeftHandMenu();            
-        });
-
-        $('#searchToggle').on('click',function(){
-            obj.toggleSearchBar();            
-        });
-
-        $('#slideNavigationRight .close').on('click',function(){            
-            obj.closeRightHandMenu();            
-        });
-
-        $('#slideNavigationLeft .close').on('click',function(){
-            obj.closeLeftHandMenu();            
-        });
-
-        $(window).on('resize',function(){
-            obj.resizeNavigationContainers();
-        }).resize();
-
-        var inertiaTimeout = 0;
-
-        $(window).on('scroll',function(){
-            var scrollPos = $(this).scrollTop();
-            obj.checkNavPosition(scrollPos);
-            
-            clearTimeout(inertiaTimeout);
-            inertiaTimeout = setTimeout(function(){ obj.resetScroll(scrollPos); },2000);
-        }).scroll();        
-    };
-
-
-    //
-    // RESET CURRENTSCROLLTOP AFTER 2 SECONDS
-    //
-
-
-    obj.resetScroll =  function(_scroll){
-        currentScrollTop = _scroll;
-        
-    };
-
-
-    //
-    // CHECK SCROLL POSITION TO SEE IF WE SHOULD SHOW / HIDE THE NAV
-    //
-
-
-    var inertiaBlocker;
-
-    obj.checkNavPosition = function(_scroll){
-        var minScrollDistance = 100;        
-
-        if(Utilities.winWidth > 992){
-            return false;
-        }
-
-        // FIX FOR APPLE'S INERTIA STUFF
-
-        if(_scroll <= 0){
-            clearTimeout(inertiaBlocker);            
-            inertiaBlocker = setTimeout(function(){
-                obj.checkNavPosition(_scroll);                
-            },2000);
-            return false;
-        }
-
-        // Show or hide the header if user has scrolled more than x-pixels
-
-        if(_scroll > currentScrollTop){
-            
-            if((_scroll - currentScrollTop) > minScrollDistance){
-                if(!$('nav').hasClass('navHidden')){
-                    TweenLite.to($('nav'),0.5,{top:-200,ease:Power4.easeIn});        
-                    $('nav').addClass('navHidden');        
-                }
-                currentScrollTop = _scroll;
-            }
-        }
-        else{
-            if((currentScrollTop - _scroll ) > minScrollDistance){
-                if($('nav').hasClass('navHidden')){
-                    TweenLite.to($('nav'),0.5,{top:0,ease:Power4.easeOut});                
-                    $('nav').removeClass('navHidden');
-                }
-                currentScrollTop = _scroll;                
-            }
-        }
-    };    
-
-
-
-    // 
-    //  SETTING ACTIVE MENU ITEMS
-    //
-
-
-    obj.setActiveMenuItem = function(_el){
-        $('.menuToggleButton').removeClass('active');    
-
-        if(_el || _el !== undefined){
-            $(_el).addClass('active');
-        }
-    };
-
-
-    //
-    // RIGHT HAND MENU
-    //
-
-
-    obj.openRightHandMenu = function(){
-        $('#slideNavigationRight').addClass('open');
-        obj.showOverlay();
-    };
-
-
-    obj.closeRightHandMenu = function(){
-        $('#slideNavigationRight').removeClass('open');
-
-        // ONLY HIDE OVERLAY IF BOTH SLIDENAVIGATIONS ARE CLOSED        
-        if(!$('#slideNavigationLeft').hasClass('open')) obj.hideOverlay();
-    };
-
-
-    obj.toggleRightHandMenu = function(){
-        if($('#slideNavigationRight').hasClass('open')){
-            obj.closeRightHandMenu();
-        }
-        else{
-            obj.openRightHandMenu();
-        }
-    };
-
-
-    //
-    // LEFT HAND MENU
-    //
-
-
-    obj.openLeftHandMenu = function(){
-        $('#slideNavigationLeft').addClass('open');
-        obj.showOverlay();
-    };
-
-
-    obj.closeLeftHandMenu = function(){
-        $('#slideNavigationLeft').removeClass('open');
-
-        // ONLY HIDE OVERLAY IF BOTH SLIDENAVIGATIONS ARE CLOSED
-        if(!$('#slideNavigationRight').hasClass('open')) obj.hideOverlay();
-        
-    };
-
-
-    obj.toggleLeftHandMenu = function(){
-        if($('#slideNavigationLeft').hasClass('open')){
-            obj.closeLeftHandMenu();
-        }
-        else{
-            obj.openLeftHandMenu();
-        }
-    };    
-
-
-    //
-    // NAVIGATION OVERLAY - SHOW / HIDE
-    //
-
-    obj.showOverlay = function(){
-        $('#navigationOverlay').addClass('show');
-
-        $('#navigationOverlay').on('click',function(){
-            obj.closeLeftHandMenu();
-            obj.closeRightHandMenu();
-        });
-
-        // PREVENT BODY SCROLLING
-        $('body').addClass('hideOverflow');        
-        
-    };
-
-
-    obj.hideOverlay = function(){
-        $('#navigationOverlay').removeClass('show');
-        $('#navigationOverlay').unbind('click');        
-        
-        $('body').removeClass('hideOverflow');
-    };
-
-
-    //
-    // TOGGLE SEARCHBAR - ONLY BELOW LANDSCAPE TABLET
-    //
-
-
-    obj.toggleSearchBar = function(){
-        $('#searchContainer').css('display','block');
-    };
-
-
-    //
-    // RESIZE SLIDENAVIGATION CONTAINERS BECAUSE WE NEED A PIXELHEIGHT FOR THE INNER SCROLLBAR
-    //
-
-
-    obj.resizeNavigationContainers = function(){        
-
-        var winHeight = (typeof window.outerHeight != 'undefined') ? Math.max(window.outerHeight, $(window).height()) : $(window).height();        
-
-        if(Utilities.winWidth > 480){
-            $('#slideNavigationRight .view').css('height',$(window).height() - ($('#slideNavigationRight .view').offset().top - $(window).scrollTop()));
-            $('#slideNavigationLeft .explanationContainer').css('height',$(window).height() - ($('#slideNavigationLeft .view').offset().top - $(window).scrollTop()));
-            
-        }
-        else{
-            $('#slideNavigationRight .view').css('height',$(window).innerHeight() - ($('#slideNavigationRight .view').offset().top));
-            $('#slideNavigationRight').css('height',$(window).height());
-        }
-
-    };
-
+ 
 
     return obj;
 
--- src/js/modules/socials.js
+++ src/js/modules/socials.js
@@ -4,45 +4,9 @@
 
 Socials = (function(){
 
-    // AIzaSyBx-Su80zi4ySOQNi2QBYggIMfOdvH7mog
-
+    
     var obj = {};
 
-
-
-    obj.getShortUrl = function(_data,_callback){
-        var requestData = _data;
-
-        var request = gapi.client.urlshortener.url.insert({
-            'resource': {
-                'longUrl': requestData.longURL
-            }
-        });
-
-        request.execute(function(response){
-
-            if(response.id !== null){
-                requestData.shortURL = response.id;                
-                _callback(requestData);
-            } 
-            else{
-                // FAILED, USE UNSHORTENED URL
-                requestData.shortURL = requestData.longURL;                
-                _callback(requestData);
-            }
-        });
-
-    };
-
-
-    function initShortner() {
-        gapi.client.setApiKey('AIzaSyBx-Su80zi4ySOQNi2QBYggIMfOdvH7mog');
-        gapi.client.load('urlshortener', 'v1').then(function(){});
-    }
-
-    setTimeout(function(){
-        initShortner();
-    },2000);
 
 
 
@@ -60,25 +24,14 @@ Socials = (function(){
     // TWITTER
     //
 
-    obj.shareOnTwitter = function(_data){
+    obj.shareOnTwitter = function(_url){
 
-        var shareData = _data;
-        shareData.longURL = 'http://ezelsbruggetje.nl/' + shareData.id;
 
-        // GET THE SHORT URL
-        
-        if(_data.shortURL === undefined){
-            obj.getShortUrl(shareData, obj.shareOnTwitter);    
-            return;
-        }
-  
-        // WE GET THE TITLE OF THE LINK
-
-        var title  = escape("beschrijving bruggetje");
+        var title  = escape("example");
 
         // OPEN AND CENTER TWITTER DIALOGUE
 
-        window.open('http://twitter.com/share?url=' + _data.shortURL + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        window.open('http://twitter.com/share?url=' + _url + '&text=' + title + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
 
     };
 
@@ -88,26 +41,17 @@ Socials = (function(){
     //
 
 
-    obj.shareOnFacebook = function(_data){
+    obj.shareOnFacebook = function(_url){
         
-        var shareData = _data;
-        shareData.longURL = 'http://ezelsbruggetje.nl/' + shareData.id;
-
-        // GET THE SHORT URL
-        
-        if(_data.shortURL === undefined){
-            obj.getShortUrl(shareData, obj.shareOnFacebook);    
-            return;
-        }
 
 
         FB.ui({
             method: 'feed',
             name: 'This is the content of the "name" field.',
-            link: shareData.shortURL,
+            link: _url,
             picture: 'http://www.groupstudy.in/img/logo3.jpeg',
-            caption: 'Ezelsbruggetje.nl',
-            description: 'De omschrijving van het bruggetje',
+            caption: 'example',
+            description: 'De omschrijving van het example',
             message: ''
         });
     };
